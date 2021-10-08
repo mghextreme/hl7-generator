091bd76a2a02618f3379157e93eeac5ced249d4c
Matias G Henschel <matias.henschel@philips.com>
2021-10-08 09:43:17 -0300

    Fix template ordering. Improved OBX section.

--- src/app/models/obx-section.ts
+++ src/app/models/obx-section.ts
@@ -1,5 +1,5 @@
 import { MessageConfigurationService } from 'app/services';
-import { StringField } from './fields';
+import { DateTimeField, StringField } from './fields';
 import { MultipleField } from './multiple.field';
 import { SectionBase } from './section-base';
 import { SectionType } from './section-type.enum';
@@ -24,9 +24,21 @@ export class ObxSection extends SectionBase {
         'sections.obx.3',
         [
           new StringField(1, 'sections.obx.3.1'),
-          new StringField(2, 'sections.obx.3.2')
+          new StringField(2, 'sections.obx.3.2'),
+          new StringField(3, 'sections.obx.3.3')
         ]),
-      new StringField(5, 'sections.obx.5')
+      new StringField(5, 'sections.obx.5'),
+      new MultipleField(
+        this.configService,
+        6,
+        'sections.obx.6',
+        [
+          new StringField(1, 'sections.obx.6.1'),
+          new StringField(2, 'sections.obx.6.2'),
+          new StringField(3, 'sections.obx.6.3')
+        ]),
+      new StringField(11, 'sections.obx.11'),
+      new DateTimeField(14, 'sections.obx.14')
     ];
   }
 }
