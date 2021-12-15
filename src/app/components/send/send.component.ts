09a1751d49f8cab1804dcecc33da99e09a8c562d
Matias G Henschel <matias.henschel@philips.com>
2021-12-15 10:52:16 -0300

    Show HL7 in Send page.

--- src/app/components/send/send.component.ts
+++ src/app/components/send/send.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { MessageConfigurationService } from 'app/services';
 
 @Component({
   templateUrl: './send.component.html',
   styleUrls: ['./send.component.scss']
 })
 export class SendComponent {
+  hl7 = '';
 
+  constructor(
+    private readonly configService: MessageConfigurationService
+  ) {
+    this.hl7 = this.configService.retrieve('hl7') ?? '';
+  }
+
+  public handleHl7Change(): void {
+    this.configService.store('hl7', this.hl7);
+  }
 }
